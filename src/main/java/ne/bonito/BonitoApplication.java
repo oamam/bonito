fb88ea603aba77d9d808eaa94199a22238f47609
oamam <chapa0106@gmail.com>
2019-03-01 22:54:47 +0900

    first commit

---
+++ src/main/java/ne/bonito/BonitoApplication.java
@@ -0,0 +1,13 @@
+package ne.bonito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonitoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BonitoApplication.class, args);
+  }
+
+}
